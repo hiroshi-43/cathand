37564eb0ebc1d60bae2be25ca8441361e18f180d
hiroshi-43 <piropirorock@gmail.com>
2024-10-22 14:27:35 +0900

    外箱のアクティブハッシュ作成

---
+++ app/models/outer_box.rb
@@ -0,0 +1,14 @@
+class OuterBox < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 1, name: '試作UP' },
+    { id: 2, name: '設計校了' },
+    { id: 3, name: '出稿' },
+    { id: 4, name: '校正データUP' },
+    { id: 5, name: '版下校了' },
+    { id: 6, name: 'その他' }   
+  ]
+
+  include ActiveHash::Associations
+  has_many :projects
+end
