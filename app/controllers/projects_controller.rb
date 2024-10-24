d95ec7d69eb19858d11b5f870071a449cbfa4385
hiroshi-43 <piropirorock@gmail.com>
2024-10-24 13:48:38 +0900

    スケジュール計算機能途中１

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all
+    @projects = Project.order("created_at DESC")
   end
 
   def new
@@ -9,6 +9,29 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:project).permit(
+      :customer,
+      :item_name,
+      :capacity,
+      :schedule,
+      :category_id,
+      :total_task_id,
+      :container_task_id,
+      :package_task_id,
+      :inner_box_task_id,
+      :outer_box_task_id
+      )
   end
 
 end
--- app/models/lead_time.rb
+++ app/models/lead_time.rb
@@ -1,5 +1,5 @@
 class LeadTime < ActiveHash::Base
-  self.data = [{ id: 0, name: '---' }] + (1..365).map { |i| { id: i, name: i.to_s } } 
+  self.data = [{ id: 0, name: '---' }] + (1..365).map { |i| { id: i, name: i.to_s } } + [{ id: 366, name: 'その他' }]
 
   include ActiveHash::Associations
   has_many :projects
